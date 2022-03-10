752e80a937d978d2828f48525392fd2c8eee2556
Joseph Torsella <joseph.r.torsella@gmail.com>
2022-03-10 17:25:38 -0500

    fix: buttons now functioning correctly

--- client/containers/CardContainer.jsx
+++ client/containers/CardContainer.jsx
@@ -4,9 +4,10 @@ import MediaCard from '../components/MediaCard';
 function CardContainer(props) {
   const movies = [];
   for (let i = 0; i < props.movieList.length; i += 1) {
-    movies.push(
-      <MediaCard key={`movieList${i}`} tmdbId={props.movieList[i].TMDBid} posterPath={props.movieList[i].poster_path} originalTitle={props.movieList[i].original_title} />,
-    );
+      console.log(props.movieList[i]);
+      movies.push(
+        <MediaCard fav={props.movieList[i].fav} toWatch={props.movieList[i].toWatch} haveSeen={props.movieList[i].haveSeen} key={`MediaCard${props.movieList[i].TMDBid}`} tmdbId={props.movieList[i].TMDBid} posterPath={props.movieList[i].poster_path} originalTitle={props.movieList[i].original_title} />,
+      );
   }
   return <div className="card-container">{movies}</div>;
 }
